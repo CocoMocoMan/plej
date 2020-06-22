1bd779641da494a9ebdd614e826d05c4fd65b08d
Lorenzo Flud <lorenzoflud15@gmail.com>
2020-06-22 14:00:41 -0700

    Landing Page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -7,6 +7,7 @@ import Donate from '@/views/Donate'
 import InvalidLink from '@/views/InvalidLink'
 import PaymentSetup from '@/views/PaymentSetup'
 import LinkManager from '@/views/LinkManager'
+import Landing from '@/views/Landing'
 
 Vue.use(Router)
 
@@ -47,6 +48,11 @@ export default new Router({
       path: '/invalidlink',
       name: 'InvalidLink',
       component: InvalidLink
+    },
+    {
+      path: '/landing',
+      name: 'Landing',
+      component: Landing
     }
   ]
 })
